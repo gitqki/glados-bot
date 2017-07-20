a116c59dd7d002ed3c23ee820a5129b9afc749b8
Stefan B <st.behnert@gmail.com>
2017-07-20 23:22:41 +0200

    fixed roles

--- src/classes/helper/roles.class.ts
+++ src/classes/helper/roles.class.ts
@@ -1,6 +1,6 @@
 export class UserRoles {
 
-    Admin: any = {};
+    Admins: any = {};
     Moderator: any = {};
     Dj: any = {};
     User: any = {};
@@ -10,16 +10,16 @@ export class UserRoles {
 
     getRoles(msg) { // Define User Roles
 
-        this.Admin = msg.guild.roles.find("name", "Admin");
-        this.Moderator = msg.guild.roles.find("name", "Moderator");
+        this.Admins = msg.guild.roles.find("name", "Admins");
+        //this.Moderator = msg.guild.roles.find("name", "Moderator");
         //this.Dj = msg.guild.roles.find("name", "Dj");
         //this.User = msg.guild.roles.find("name", "User");
 
 
 
         let userRole: any = {
-            "Admin": this.Admin.name == "Admin",
-            "Moderator": this.Moderator.name == "Moderator",
+            "Admins": this.Admins.name == "Admins",
+            //"Moderator": this.Moderator.name == "Moderator",
             //"Dj": this.Dj.name == "Dj",
             //"User": this.User.name == "User"
         };
