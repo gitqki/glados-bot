d3f5384ba484b2319d1c7ac9e42949a637d5547a
Stefan B <st.behnert@gmail.com>
2017-07-22 18:31:48 +0200

    added some random fun questions.

---
+++ src/classes/commands/fun/random.class.ts
@@ -0,0 +1,32 @@
+/*@prefix: no prefix here
+ *@function: Conversation Statements
+ */
+
+export class Random {
+    constructor(){
+
+    }
+    public getCommands(msg, helper, client, ytdl, CONFIG) {
+        let cases = [
+            "waifu?",
+            "gay?",
+            "skill?"
+        ];
+        let response = {
+            waifu : Math.floor((Math.random() * 10) + 1),
+            gay : Math.floor((Math.random() * 3) + 1)
+        };
+        let gay = {
+            1: " is gay",
+            2: " cologne level.",
+            3: " is not gay"
+        };
+
+        if (msg.content === msg.mentions.members.first() + ' waifu?') {
+            msg.channel.send(msg.mentions.members.first() + " is " + response.waifu + "/10 waifu!");
+        }
+        if (msg.content === msg.mentions.members.first() + ' gay?') {
+            msg.channel.send(msg.mentions.members.first() +  gay[response.gay]);
+        }
+    }
+}
